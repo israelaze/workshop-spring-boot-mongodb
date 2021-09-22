f3bf8b6a0fd870d991541ce60a8ea3f6c0aec23a
israelaze <israelaze@gmail.com>
2021-09-22 15:48:50 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/educandoweb/wokshopmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/wokshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.educandoweb.wokshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.educandoweb.wokshopmongo.domain.Post;
 import com.educandoweb.wokshopmongo.domain.User;
+import com.educandoweb.wokshopmongo.repository.PostRepository;
 import com.educandoweb.wokshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,17 +19,27 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postrepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo.", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz agora!", maria);
+
 		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postrepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/educandoweb/wokshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.wokshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.wokshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
