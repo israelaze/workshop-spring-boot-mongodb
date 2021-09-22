a2295ca322e4b456af6e59c56edca5d29920be4c
israelaze <israelaze@gmail.com>
2021-09-22 15:02:46 -0300

    Atualização de usuário com PUT

--- src/main/java/com/educandoweb/wokshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/wokshopmongo/services/UserService.java
@@ -23,8 +23,7 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
 	}
 	
 	public User insert(User obj) {
@@ -34,14 +33,24 @@ public class UserService {
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
-		
-	
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-
 	}
+	
+	
 
 	
 }
